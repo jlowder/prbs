a0fc911a00142c86de98673e3c355a9469a9fe1f
Jason Lowdermilk <jlowdermilk@rtlogic.com>
2016-04-07 10:52:26 -0600

    First round of generator functions. Very slow at this point.

--- src/prbs.lisp
+++ src/prbs.lisp
@@ -9,7 +9,9 @@
                 :take)
   (:export :make-prbs
            :num-gen
-           :bvlist-gen))
+           :bvlist-gen
+           :bit-gen
+           :byte-gen))
 
 (in-package :prbs)
 
@@ -33,6 +35,7 @@
                  (bitbv (newbit bv)))))
 
 (defun make-prbs (n &optional (iv 2))
+  "return a lazy list of n-bit bitvectors"
   (let ((taps '(0 1))
         (init (num->bits iv n)))
     (labels ((rec (v)
@@ -43,7 +46,26 @@
                             (rec next))))))
       (rec init))))
 
+(defun bit-gen (n)
+  "return a function representing prbs-N. The function takes a single argument which is the number of the next bits to generate from the sequence (default 1 bit)."
+  (let ((gen (bvlist-gen n))
+        (res #*))
+    (labels ((rec (&optional (c 1))
+               (let* ((len (length res))
+                      (num (if (>= len c)
+                               0
+                               (ceiling (/ (- c len) n))))
+                      (v (apply #'concatenate 'bit-vector res (funcall gen num))))
+                 (if (> (length v) (1+ c))
+                     (setq res (subseq v (1+ c)))
+                     (setq res #*))
+                 (if (> (length v) c)
+                     (subseq v 0 c)
+                     v))))
+    #'rec)))
+
 (defun bvlist-gen (n)
+  "return a function representing prbs-N. The function takes a single argument which is the number of the next n-bit bit vectors to generate from the sequence (default 1 value)."
   (let ((v (make-prbs n)))
     (lambda (&optional (c 1))
       (loop for i from 1 to c collecting
@@ -52,6 +74,7 @@
              b)))))
       
 (defun num-gen (n)
+  "return a function representing prbs-N. The function takes a single argument which is the number of the next n-bit integer values to generate from the sequence (default 1 value)."
   (let ((v (make-prbs n)))
     (labels ((rec (&optional (c 1))
                   (when (> c 0)
@@ -60,6 +83,14 @@
                         (setq v (dcdr v))
                         (cons (bv2int b) (rec (1- c))))))))
       #'rec)))
+
+(defun byte-gen (n)
+  "return a function representing prbs-N. The function takes a single argument which is the number of the next unsigned integer values to generate from the sequence (default 1 value).
+The final byte will be zero-padded if necessary."
+  (let ((gen (bit-gen n)))
+    (lambda (&optional (c 1))
+      (loop repeat c
+           collect (bv2int (funcall gen 8))))))
                               
 (defun bv2int (b &optional (m 1) (a 0))
   (when b
