a67a658b2c52d5e9cbf5e8ea56b18b2083648378
Jason Lowdermilk <jlowdermilk@rtlogic.com>
2016-04-11 17:08:22 -0600

    moving finder functions to another rep

--- src/prbs.lisp
+++ src/prbs.lisp
@@ -22,15 +22,13 @@
   "=> bit-vector
 
 left-shift `BV` by one bit and apply `TAPS` to generate a new right-side bit."
-  (flet ((newbit (bv)
-           (apply #'logxor (match taps
-                             ((t1 t2) (list (elt bv t1) (elt bv t2)))
-                             ((t1 t2 t3 t4) (list (elt bv t1) (elt bv t2)
-                                                  (elt bv t3) (elt bv t4)))))))
-    (concatenate 'bit-vector
-                 (subseq bv 1)
-                 (bitbv (newbit bv)))))
-  
+  (concatenate 'bit-vector
+               (subseq bv 1)
+               (bitbv (match taps
+                        ((t1 t2) (logxor (elt bv t1) (elt bv t2)))
+                        ((t1 t2 t3 t4) (logxor (elt bv t1) (elt bv t2)
+                                               (elt bv t3) (elt bv t4)))))))
+
 (defun make-prbs (n &key (seed 2))
   "=> lambda ()
 
@@ -122,27 +120,3 @@ that bit offset."
 (defun seq-length (n)
   "=> number of bits in PRBS-`N`"
   (* n (1- (expt 2 n))))
-
-(defun sfind (p n)
-  "=> bit offset where `P` occurs in PRBS-`N`, or nil if not found"
-  (let ((gen (bit-gen n)))
-    (labels ((rec (p rem n o need)
-               (if (zerop n)
-                   nil
-                   (let ((v (concatenate 'bit-vector rem (funcall gen need))))
-                     (if (equal p v)
-                         o
-                         (rec p (subseq v 1) (1- n) (1+ o) 1))))))
-      (rec p #* (seq-length n) 0 (length p)))))
-
-(defun sfind-all (p n)
-  "=> list of all bit-offsets where `P` occurs in PRBS-`N`"
-  (let ((gen (bit-gen n)))
-    (labels ((rec (p rem n o c need)
-               (if (zerop n)
-                   c
-                   (let ((v (concatenate 'bit-vector rem (funcall gen need))))
-                     (if (equal p v)
-                         (rec p (subseq v 1) (1- n) (1+ o) (cons o c) 1)
-                         (rec p (subseq v 1) (1- n) (1+ o) c 1))))))
-      (rec p #* (seq-length n) 0 nil (length p)))))
